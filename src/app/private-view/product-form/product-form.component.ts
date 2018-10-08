d7479bc3920c25175e6f16d245e6d79914f9086a
Rahman Adewale Hafeez <superdwale@gmail.com>
2018-10-08 14:33:30 +0100

    delete product done

--- src/app/private-view/product-form/product-form.component.ts
+++ src/app/private-view/product-form/product-form.component.ts
@@ -28,6 +28,14 @@ export class ProductFormComponent implements OnInit {
 
   ngOnInit() {
   }
+  deleteProduct() {
+    if (!confirm('Are you sure you want to delete this product?')) {
+      this.productService.deleteProduct(this.id);
+      this.router.navigate(['/admin/products/']);
+    } else {
+      return;
+    }
+  }
 
   saveProduct(product) {
     if (this.id) {
